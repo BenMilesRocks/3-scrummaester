4e4e18e1f816f2625672d98259f2995558e3746e
Ben Miles <benmilesGM@gmail.com>
2025-02-18 15:40:02 +0000

    updates scrummaester.sql to match app classes

--- scrummaester.sql
+++ scrummaester.sql
@@ -1,40 +1,69 @@
-CREATE TABLE "teams" (
-    "team_id" BIGSERIAL PRIMARY KEY,
-    "team_lead" VARCHAR(50) NOT NULL,
-    "team_lead_email" VARCHAR(50) NOT NULL,
+CREATE TABLE IF NOT EXISTS public.tasks
+(
+    task_id bigint NOT NULL DEFAULT nextval('tasks_task_id_seq'::regclass),
+    task_name character varying(50) COLLATE pg_catalog."default" NOT NULL,
+    task_description character varying(3000) COLLATE pg_catalog."default" NOT NULL,
+    task_status character varying(30) COLLATE pg_catalog."default" NOT NULL,
+    project_id bigint NOT NULL,
+    assigned_user bigint NOT NULL,
+    CONSTRAINT tasks_pkey PRIMARY KEY (task_id),
+    CONSTRAINT tasks_assigned_user_fkey FOREIGN KEY (assigned_user)
+        REFERENCES public.users (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION,
+    CONSTRAINT tasks_project_id_fkey FOREIGN KEY (project_id)
+        REFERENCES public.projects (project_id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
 );
 
-CREATE TABLE "users" (
-    "user_id" BIGSERIAL,
-    "first_name" VARCHAR(20) NOT NULL,
-    "last_name" VARCHAR(20) NOT NULL,
-    "username" VARCHAR(20) UNIQUE PRIMARY KEY NOT NULL,
-    "password" VARCHAR(80) NOT NULL,
-    "email" VARCHAR(50) UNIQUE NOT NULL,
-    "role" VARCHAR(20) NOT NULL,
-    "team_id" INTEGER 
-        REFERENCES "teams" ("team_id") 
-        FOREIGN KEY ON DELETE SET NULL
+CREATE TABLE IF NOT EXISTS public.users
+(
+    id bigint NOT NULL DEFAULT nextval('users_id_seq'::regclass),
+    first_name character varying(30) COLLATE pg_catalog."default" NOT NULL,
+    last_name character varying(30) COLLATE pg_catalog."default" NOT NULL,
+    username character varying(30) COLLATE pg_catalog."default" NOT NULL,
+    password character varying(100) COLLATE pg_catalog."default" NOT NULL,
+    email character varying(80) COLLATE pg_catalog."default" NOT NULL,
+    team_role character varying(80) COLLATE pg_catalog."default" NOT NULL,
+    team_id bigint NOT NULL,
+    CONSTRAINT users_pkey PRIMARY KEY (id),
+    CONSTRAINT users_email_key UNIQUE (email),
+    CONSTRAINT users_username_key UNIQUE (username),
+    CONSTRAINT users_team_id_fkey FOREIGN KEY (team_id)
+        REFERENCES public.teams (team_id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
 );
 
-CREATE TABLE "projects" (
-    "project_id" BIGSERIAL PRIMARY KEY,
-    "project_name" VARCHAR(50) NOT NULL,
-    "project_status" VARCHAR(50) NOT NULL,
-    "team_id" INTEGER 
-        REFERENCES "teams" ("team_id") 
-        FOREIGN KEY ON DELETE SET NULL
+CREATE TABLE IF NOT EXISTS public.projects
+(
+    project_id bigint NOT NULL DEFAULT nextval('projects_project_id_seq'::regclass),
+    project_name character varying(30) COLLATE pg_catalog."default" NOT NULL,
+    project_status character varying(30) COLLATE pg_catalog."default" NOT NULL,
+    team_id bigint NOT NULL,
+    CONSTRAINT projects_pkey PRIMARY KEY (project_id),
+    CONSTRAINT projects_team_id_fkey FOREIGN KEY (team_id)
+        REFERENCES public.teams (team_id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
 );
 
-CREATE TABLE "tasks" (
-    "task_id" BIGSERIAL PRIMARY KEY,
-    "task_name" VARCHAR(50) UNIQUE NOT NULL,
-    "task_description" VARCHAR(1000),
-    "status" VARCHAR(50) NOT NULL,
-    "project_id" INTEGER 
-        REFERENCES "projects" ("project_id") 
-        FOREIGN KEY ON DELETE CASCADE,
-    "assigned_user" VARCHAR 
-        REFERENCES "users" ("username") 
-        FOREIGN KEY ON DELETE SET NULL
+CREATE TABLE IF NOT EXISTS public.tasks
+(
+    task_id bigint NOT NULL DEFAULT nextval('tasks_task_id_seq'::regclass),
+    task_name character varying(50) COLLATE pg_catalog."default" NOT NULL,
+    task_description character varying(3000) COLLATE pg_catalog."default" NOT NULL,
+    task_status character varying(30) COLLATE pg_catalog."default" NOT NULL,
+    project_id bigint NOT NULL,
+    assigned_user bigint NOT NULL,
+    CONSTRAINT tasks_pkey PRIMARY KEY (task_id),
+    CONSTRAINT tasks_assigned_user_fkey FOREIGN KEY (assigned_user)
+        REFERENCES public.users (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION,
+    CONSTRAINT tasks_project_id_fkey FOREIGN KEY (project_id)
+        REFERENCES public.projects (project_id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
 );
