b04bb5b77875170aa43de8c7924ef9f40f53e0fa
Ben Miles <benmilesGM@gmail.com>
2025-02-18 16:16:45 +0000

    corrects sql file to include teams table

--- scrummaester.sql
+++ scrummaester.sql
@@ -1,21 +1,15 @@
-CREATE TABLE IF NOT EXISTS public.tasks
+CREATE TABLE IF NOT EXISTS public.teams
 (
-    task_id bigint NOT NULL DEFAULT nextval('tasks_task_id_seq'::regclass),
-    task_name character varying(50) COLLATE pg_catalog."default" NOT NULL,
-    task_description character varying(3000) COLLATE pg_catalog."default" NOT NULL,
-    task_status character varying(30) COLLATE pg_catalog."default" NOT NULL,
-    project_id bigint NOT NULL,
-    assigned_user bigint NOT NULL,
-    CONSTRAINT tasks_pkey PRIMARY KEY (task_id),
-    CONSTRAINT tasks_assigned_user_fkey FOREIGN KEY (assigned_user)
-        REFERENCES public.users (id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION,
-    CONSTRAINT tasks_project_id_fkey FOREIGN KEY (project_id)
-        REFERENCES public.projects (project_id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-);
+    team_id bigint NOT NULL DEFAULT nextval('teams_team_id_seq'::regclass),
+    team_lead character varying(30) COLLATE pg_catalog."default" NOT NULL,
+    team_lead_email character varying(30) COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT teams_pkey PRIMARY KEY (team_id)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.teams
+    OWNER to postgres;
 
 CREATE TABLE IF NOT EXISTS public.users
 (
