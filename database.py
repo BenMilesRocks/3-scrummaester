ed2fdc56d860792cfc3992d70637775db6c523b6
Ben Miles <benmilesGM@gmail.com>
2025-02-24 15:45:22 +0000

    fixes query syntax in database.py

--- database.py
+++ database.py
@@ -1,4 +1,5 @@
-from __init__ import session, login_manager, app
+import sqlalchemy
+from __init__ import session, login_manager, app, engine
 from models import Team, User, Project, Task
 
 team_db = session.query(Team)
@@ -9,43 +10,42 @@ task_db = session.query(Task)
 #Project by Team
 
 def project_by_team(team):
-    team_projects = session.query(project_db).filter_by(team_id = team)
+    team_projects = session.query(Project).filter_by(team_id = team)
     for project in team_projects:
         return project
 
 #Task by Project
 
 def task_by_project(project):
-    project_tasks = session.query(task_db).filter_by(project_id = project)
+    project_tasks = session.query(Task).filter_by(project_id = project)
     for task in project_tasks:
         return task
 
 #User by Team
 
 def user_by_team(team):
-    team_users = session.query(user_db).filter_by(team_id = team)
+    team_users = session.query(User).filter_by(team_id = team)
     for user in team_users:
         return user
     
 #Task by User
 
 def task_by_user(user):
-    user_tasks = session.query(task_db).filter_by(user_id = user)
+    user_tasks = session.query(Task).filter_by(user_id = user)
     for task in user_tasks:
         return task
 
 #User by Project
 
 def user_by_project(project):
-    project_users = session.query(user_db).filter_by(project_id = project)
+    project_users = session.query(User).filter_by(project_id = project)
     for user in project_users:
         return user
 
 #Project by User
 
 def project_by_user(user):
-    user_projects = session.query(project_db).filter_by(user_id = user)
+    user_projects = session.query(Project).filter_by(user_id = user)
     for project in user_projects:
         return project
-    
     
