914f74b12ebade60aa13cc399b4315bb6e84025b
Ben Miles <benmilesGM@gmail.com>
2025-03-23 23:40:10 +0000

    applies linting to forms.py

--- forms.py
+++ forms.py
@@ -1,14 +1,16 @@
 from __init__ import session
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import InputRequired, Length, ValidationError, DataRequired
+from wtforms.validators import (
+    InputRequired, Length, ValidationError, DataRequired
+    )
 from wtforms_sqlalchemy.fields import QuerySelectField
 import re
 
 from models import User, Team
 from database import team_db
 
-from models import User
+
 class RegistrationForm(FlaskForm):
     first_name = StringField(validators=[InputRequired(), DataRequired(), Length(min=1, max=30)], render_kw={"placeholder":"First Name"})
     last_name = StringField(validators=[InputRequired(), DataRequired(), Length(min=1, max=30)], render_kw={"placeholder":"Last Name"})
@@ -17,18 +19,17 @@ class RegistrationForm(FlaskForm):
     email = StringField(validators=[InputRequired(), DataRequired(), Length(min=4, max=30)], render_kw={"placeholder":"Email Address"})
     team_role = StringField(validators=[InputRequired(), DataRequired(), Length(min=4, max=30)], render_kw={"placeholder":"Job Role"})
     team_id = QuerySelectField(query_factory=lambda: session.query(Team).all())
-
-
     submit = SubmitField("Register")
 
     def validate_username(self, username):
         existing_user_name = session.query(User).filter_by(
-            username = username.data).first()
+            username=username.data).first()
         if existing_user_name:
             raise ValidationError(
                 "That username already exists. Please choose a different one"
             )
-        
+
+
 class LoginForm(FlaskForm):
     username = StringField(validators=[InputRequired(), Length(min=4, max=30)], render_kw={"placeholder":"Username"})
     password = PasswordField(validators=[InputRequired(), Length(min=4, max=30)], render_kw={"placeholder":"Password"})
